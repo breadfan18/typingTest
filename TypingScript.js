f52262a4d624f50fe2a74cb8291bba7c099faa2f
Swaroop <breadfan18@gmail.com>
2021-01-26 14:54:28 -0500

    working on timer math

--- TypingScript.js
+++ TypingScript.js
@@ -1,2 +1,37 @@
 const originText = document.querySelector("#origin-text p").innerHTML;
-console.log(originText);
+const theTimer = document.querySelector(".timer");
+const resetButton = document.querySelector("#reset");
+const testArea = document.querySelector("#test-area");
+const testWrapper = document.querySelector(".test-wrapper");
+
+
+let timer = [0,0,0,0];
+
+//Start the timer
+function runTimer() {
+    let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2];
+    theTimer.innerHTML = currentTime;
+    timer[3]++;
+
+    timer[0] = Math.floor()
+ }
+
+
+//Text Validation
+
+
+
+//Reset Everything
+
+
+
+//Intervals
+function start() {
+    setInterval(runTimer, 10);
+}
+
+
+
+
+//Event listeners
+testArea.addEventListener("keypress", start, false);
