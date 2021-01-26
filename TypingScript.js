285979a07fa3f3029fc7f41a76a6ef2ecf67b161
Swaroop <breadfan18@gmail.com>
2021-01-26 15:31:28 -0500

    got the basics of the spellcheck function down

--- TypingScript.js
+++ TypingScript.js
@@ -7,17 +7,39 @@ const testWrapper = document.querySelector(".test-wrapper");
 
 let timer = [0,0,0,0];
 
+//add leading zeroes to timer
+
+
 //Start the timer
 function runTimer() {
     let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2];
     theTimer.innerHTML = currentTime;
     timer[3]++;
 
-    timer[0] = Math.floor()
+    timer[0] = Math.floor((timer[3]/100)/60);
+    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
+    timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
  }
 
 
 //Text Validation
+function spellCheck() {
+    let currentText = testArea.value;
+    console.log(currentText);
+
+    let originTextSub = originText.substring(0, currentText.length);
+    console.log(originTextSub);
+
+    if (currentText === originText) {
+        testArea.style.borderColor = "#429890";
+    } else {
+        if (currentText !== originTextSub) {
+            testArea.style.borderColor = "red";
+        } else {
+            testArea.style.borderColor = "blue";
+        }
+    }
+}
 
 
 
@@ -35,3 +57,4 @@ function start() {
 
 //Event listeners
 testArea.addEventListener("keypress", start, false);
+testArea.addEventListener("keyup", spellCheck, false);
